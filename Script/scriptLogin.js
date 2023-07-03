a86c06e7819a18f7c39c33128bf0d3e88dfe6f0f
HoangVu <vudh.22itb@vku.udn.vn>
2023-07-03 12:08:31 +0700

    backup 13

--- Script/scriptLogin.js
+++ Script/scriptLogin.js
@@ -19,3 +19,35 @@ btnPopup.addEventListener("click", () => {
 iconClose.addEventListener("click", () => {
   wrapper.classList.remove("active-popup");
 });
+
+function signup(e) {
+  event.preventDefault();
+  var username = document.getElementById("usernamere").value;
+  var email = document.getElementById("emailre").value;
+  var password = document.getElementById("passwordre").value;
+  var user = {
+    username: username,
+    email: email,
+    password: password,
+  };
+  var json = JSON.stringify(user);
+  localStorage.setItem(email, json);
+  alert("đăng ký thành công");
+}
+function login(e) {
+  event.preventDefault();
+  var email = document.getElementById("emaillg").value;
+  var password = document.getElementById("passwordlg").value;
+  var user = localStorage.getItem(email);
+  if (user) {
+    var data = JSON.parse(user);
+    if (email == data.email && password == data.password) {
+      alert("Đăng nhập thành công");
+      window.location.href = "./../index.html";
+    } else {
+      alert("Đăng nhập thất bại!");
+    }
+  } else {
+    alert("Tài khoản không tồn tại!");
+  }
+}
